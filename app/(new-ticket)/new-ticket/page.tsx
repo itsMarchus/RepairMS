bb18e66b486dce5882b3d36796802ecf8f042649
itzD3L <marchuso.deligero@gmail.com>
2026-03-17 23:45:01 +0800

    Refactor ticketing and settings structure by removing old components and adding new pages for ticket management and settings under the submain directory. Introduced a new layout component and enhanced the TicketAdd component with a detailed form for customer and device information.

--- app/(new-ticket)/new-ticket/page.tsx
+++
@@ -1,9 +0,0 @@
-import TicketAdd from "@/app/components/ui/tickets/ticketAdd";
-
-export default function NewTicket() {
-    return (
-        <div>
-            <h1>New Ticket</h1>
-        </div>
-    );
-}
